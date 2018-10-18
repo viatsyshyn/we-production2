1b3db046b25536209029363c68b461f96285ab9a
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2018-10-18 22:56:13 +0300

    fix

--- ui/src/pages/home/about-us.js
+++ ui/src/pages/home/about-us.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Translate } from "react-localize-redux";
 
+import { ActiveLink } from '../../controls';
+
 import { fromEvent } from 'rxjs';
 import { map } from "rxjs/operators/index";
 
@@ -22,7 +24,7 @@ export default class AboutUs extends React.Component {
                     const height = window.innerWidth * .56;
                     const currentScrollPos = window.pageYOffset;
                     this.aboutUsRef.current.style.transform = `translate(0, ${top > 10 ? Math.min(0, currentScrollPos - height) : 0}px)`;
-                    this.aboutUsRef.current.style.opacity = Math.min(1, currentScrollPos / height * 2);
+                    this.aboutUsRef.current.style.opacity = top > 10 ? Math.min(1, .3 + currentScrollPos / height * 2) : 0;
                 })
             )
             .subscribe();
@@ -49,6 +51,11 @@ export default class AboutUs extends React.Component {
                             <li><Translate id="home.about-us.b"/></li>
                             <li><Translate id="home.about-us.c"/></li>
                         </ul>
+                    </p>
+                    <p className="content">
+                        <ActiveLink to="/contact" className="button is-primary is-rounded">
+                            <Translate id="home.about-us.order-now"/>
+                        </ActiveLink>
                     </p>
                 </div>
             </section>
